8fa16ac0dade5f0296865f630c5a9adb63cedb98
hoskliza <48154585+hoskliza@users.noreply.github.com>
2019-03-02 15:51:34 +0300

    Add files via upload

---
+++ races/cmake-build-debug/CMakeFiles/races.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/races.dir/main.cpp.o"
+  "races.pdb"
+  "races"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/races.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
